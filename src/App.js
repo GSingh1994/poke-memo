a7fb7baf56da672d1d366d6af4b4c9c3fe40a6d8
GSingh1994 <singh.gagandeep0034@gmail.com>
2021-05-18 00:53:47 -0500

    ✨ Add pokeApi

--- src/App.js
+++ src/App.js
@@ -2,29 +2,26 @@ import "./App.css";
 import { useState, useEffect } from "react";
 
 function App() {
-  const [pokeUrl, setPokeUrl] = useState([]);
+  const getRandomNums = (num) => Math.floor(Math.random() * num);
   const [pokeData, setPokeData] = useState([]);
+  const [totalPokemons, setTotalPokemons] = useState(40);
   useEffect(() => {
     const fetchUrl = async () => {
-      const response = await fetch("https://pokeapi.co/api/v2/pokemon?limit=5");
-      const urls = await response.json();
-      setPokeUrl(urls.results.map((poke) => poke.url));
+      for (let i = 1; i < totalPokemons; i += 3) {
+        let response = await fetch(`https://pokeapi.co/api/v2/pokemon/${i}/`);
+        const data = await response.json();
+        setPokeData((preData) => [...preData, data]);
+      }
     };
     fetchUrl();
   }, []);
 
-  // useEffect(()=>{
-  //   const fetchData=async()=>{
-  //     const response=await
-  //   }
-  // },[])
-
   return (
     <div className="App">
       <main className="container">
-        {/* {data.map((got) => (
-          <img key={got.id} src={got.imageUrl} alt={got.title} />
-        ))} */}
+        {pokeData.map((poke, i) => (
+          <img key={i} src={poke.sprites.front_default} alt="#" />
+        ))}
       </main>
     </div>
   );
