45238c40eaa7b5cae0a609bef85a87c154056ab4
GSingh1994 <singh.gagandeep0034@gmail.com>
2021-05-20 10:43:26 -0500

    ✨ Add gh-pages

--- src/Components/ScoreBoard.jsx
+++ src/Components/ScoreBoard.jsx
@@ -9,10 +9,13 @@ export default function ScoreBoard({ userScore, gameOver }) {
   return (
     <div className="ScoreBoard">
       <button type="button" className="nes-btn is-primary">
-        <h2 className="userScore">Score:{userScore}</h2>
+        {!gameOver ? (
+          <h2 className="userScore">Score:{userScore}</h2>
+        ) : (
+          <h2 className="gameOver">{gameOver ? "GAME OVER" : null}</h2>
+        )}
         <h2>HighScore:{gameOver ? prevScore : 0}</h2>
       </button>
-      <h2>{gameOver ? "GAME OVER" : null}</h2>
     </div>
   );
 }
