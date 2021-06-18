5408f5d2e09d1046dd7893fd3e601bc6501120c0
GSingh1994 <singh.gagandeep0034@gmail.com>
2021-06-18 10:51:46 -0500

    🐛 change grid to responsive

--- src/Components/Cards.jsx
+++ src/Components/Cards.jsx
@@ -1,21 +1,14 @@
-import { useState } from "react";
-import { CSSTransition } from "react-transition-group";
-
 export default function Cards({ sprite, name, id, saveCard }) {
-  const [inProp, setInProp] = useState(false);
-
   return (
-    <div className="cards" onClick={() => setInProp(true)}>
-      <CSSTransition in={inProp} timeout={200} classNames="my-node">
-        <div
-          id="Cards"
-          className="nes-pointer nes-container with-title is-centered is-rounded"
-          onClick={() => saveCard(id)}
-        >
-          <p className="title">{name}</p>
-          <img className="sprite" src={sprite} alt="" />
-        </div>
-      </CSSTransition>
+    <div className="cards">
+      <div
+        id="Cards"
+        className="nes-pointer nes-container with-title is-centered is-rounded"
+        onClick={() => saveCard(id)}
+      >
+        <p className="title">{name}</p>
+        <img className="sprite" src={sprite} alt="" />
+      </div>
     </div>
   );
 }
